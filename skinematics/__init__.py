9710388181e4b064a9e37b9191b76286dfcbfbac
Sebastian Luque <sluque@wanderer>
2018-10-04 13:57:32 -0500

    Simplify imports and make leaner package structure
    includes initial clean-up to improve readability.

--- skinematics/__init__.py
+++ skinematics/__init__.py
@@ -1,6 +1,5 @@
-'''
-"scikit-kinematics" primarily contains functions for working with 3D kinematics. (i.e.
-quaternions and rotation matrices).
+""""Functions for working with 3D kinematics (i.e. quaternions and rotation
+matrices)
 
 Compatible Python 3.
 
@@ -12,13 +11,13 @@ Homepage
 --------
 http://work.thaslwanter.at/skinematics/html/
 
-Copyright (c) 2018 Thomas Haslwanter <thomas.haslwanter@fh-ooe.at>
+Copyright (c) 2019 Thomas Haslwanter <thomas.haslwanter@fh-ooe.at>
 
-'''
+"""
 
 import importlib
 
-__author__ = "Thomas Haslwanter <thomas.haslwanter@fh-linz.at"
+__author__ = "Thomas Haslwanter <thomas.haslwanter@fh-linz.at>"
 __license__ = "BSD 2-Clause License"
 __version__ = "0.8.3"
 
@@ -36,5 +35,5 @@ for _m in optional_imports:
         importlib.import_module('.'+_m, package='skinematics')
         __all__.append(_m)
     except ModuleNotFoundError:
-        print("Failed to import optional module {}. Install optional dependencies".format(_m))
-
+        print(("Failed to import optional module {}. "
+               "Install optional dependencies").format(_m))
